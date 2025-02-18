a120e6924f4103f5ef45f3eee82e1ca3ae454396
dr-winner <duvorrichardwinner@gmail.com>
2025-02-18 01:46:54 -0700

    index add to list

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -50,12 +50,12 @@ export const ProjectsSection = () => {
   return (
     <section className="pb-16 lg:py-24">
       <div className="container">
-         <SectionHeader
-                eyebrow="Real World Results"
-                title="Featured Projects"
-                description="Explore how I trarnsform mere concepts into super engaging digital
+        <SectionHeader
+          eyebrow="Real World Results"
+          title="Featured Projects"
+          description="Explore how I trarnsform mere concepts into super engaging digital
           innovatons."
-              />        
+        />
         <div className="flex md:mt-20 flex-col mt-10 gap-20">
           {portfolioProjects.map((project) => (
             <div
@@ -70,37 +70,40 @@ export const ProjectsSection = () => {
               ></div>
               <div className="lg:grid lg:grid-cols-2 lg:gap-16">
                 <div className="lg:pb-16">
-                <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text p-8">
-                  <span>{project.company}</span>
-                  <span>&bull;</span>
-                  <span>{project.year}</span>
-                </div>
+                  <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text p-8">
+                    <span>{project.company}</span>
+                    <span>&bull;</span>
+                    <span>{project.year}</span>
+                  </div>
 
-                <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl">
-                  {project.title}
-                </h3>
-                <hr className="border-t-2 border-white/15 mt-4 md:mt-5" />
-                <ul className="flex flex-col gap-4 mt-4 md:mt-5">
-                  {project.results.map((result) => (
-                    <li key={project.id} className="flex gap-2 text-sm text-white/50 md:text-base">
-                      <CheckIcon className="size-5 md:size-6" />
-                      <span>{result.title}</span>
-                    </li>
-                  ))}
-                </ul>
-                <a href={project.link}>
-                  <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
-                    <span>View Project</span>
-                    <ArrowUpRightIcon className="size-4" />
-                  </button>
-                </a>
+                  <h3 className="font-serif text-2xl mt-2 md:mt-5 md:text-4xl">
+                    {project.title}
+                  </h3>
+                  <hr className="border-t-2 border-white/15 mt-4 md:mt-5" />
+                  <ul className="flex flex-col gap-4 mt-4 md:mt-5">
+                    {project.results.map((result, index) => (
+                      <li
+                        key={index}
+                        className="flex gap-2 text-sm text-white/50 md:text-base"
+                      >
+                        <CheckIcon className="size-5 md:size-6" />
+                        <span>{result.title}</span>
+                      </li>
+                    ))}
+                  </ul>
+                  <a href={project.link}>
+                    <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+                      <span>View Project</span>
+                      <ArrowUpRightIcon className="size-4" />
+                    </button>
+                  </a>
                 </div>
                 <div className="relative">
-                <Image
-                  src={project.image}
-                  alt={project.title}
-                  className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"
-                />
+                  <Image
+                    src={project.image}
+                    alt={project.title}
+                    className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"
+                  />
                 </div>
               </div>
             </div>
